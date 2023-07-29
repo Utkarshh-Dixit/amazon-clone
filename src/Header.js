8da33e166f19aac7f6621520fcc295ae338f960d
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2023-07-29 15:51:49 +0530

    wow

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 function Header() {
   return (
@@ -7,33 +9,38 @@ function Header() {
          <img className='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='kch nhi.'/>
          <div className='header_search'>
             <input className='header_searchInput' type='text'/>
+            <SearchIcon className='header_searchIcon'/>
          </div>
          <div className='header_nav'>
             <div className='header_option'>
-                <span className='header_option1'>
+                <span className='header_optionOne'>
                    Hello User 
                 </span>
-                <span className='header_option1'>
+                <span className='header_optionTwo'>
                     Sign in
                 </span>
             </div>
             <div className='header_option'>
-                <span className='header_option1'>
+                <span className='header_optionOne'>
                    Returns 
                 </span>
-                <span className='header_option1'>
+                <span className='header_optionTwo'>
                     & Orders
                 </span>
 
             </div>
             <div className='header_option'>
-                <span className='header_option1'>
+                <span className='header_optionOne'>
                    Your 
                 </span>
-                <span className='header_option1'>
+                <span className='header_optionTwo'>
                     Prime
                 </span>
 
+            </div>
+            <div className='header_basket'>
+                <ShoppingCartOutlinedIcon/>
+                <span className='header_basketCount'>0</span>
             </div>
          </div>
     </div>
